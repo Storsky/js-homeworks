a9956859ce3b9788c96124f1aa6a764aae84d4d5
Storsky <andrkvasn@yandex.ru>
2023-04-10 19:43:29 +0300

    event-object

--- js 4/4-3/task.js
+++ js 4/4-3/task.js
@@ -1,73 +1,27 @@
-const bookControl= document.querySelector(".book")
-const fontControl= document.querySelector(".book__control_font-size")
-const colorControl= document.querySelector(".book__control_color")
-const bgControl= document.querySelector(".book__control_background")
-const fonts = Array.from(document.querySelectorAll(".font-size"))
-const colors = (Array.from(colorControl.children)).slice(1)
-const bgColors = (Array.from(bgControl.children)).slice(1)
+const tabNav = document.querySelector('.tab__navigation');
+const tabs = tabNav.querySelectorAll('.tab');
+const tabContents = document.querySelectorAll('.tab__content');
 
-const fontsizeChange = function(event){
-   let index = fonts.findIndex((font) =>
-   font.classList.contains("font-size_active"));
-   let target = event.target;
-   event.preventDefault();
-
-fonts[index].classList.remove("font-size_active");
-target.classList.toggle("font-size_active")
-bookControl.classList.remove("book_fs-small", "book_fs-big")
-if (target.classList.contains("font-size_small")){
-    bookControl.classList.add("book_fs-small")  
-} else if (target.classList.contains("font-size_big")){
-    bookControl.classList.add("book_fs-big")
-}
-}
-
-
-const colorChange = function(event){
-    if (event.target.tagName == 'SPAN') {
-        return
+function activateTab(tabIndex) {
+  tabs.forEach((tab, index) => {
+    if (index === tabIndex) {
+      tab.classList.add('tab_active');
+    } else {
+      tab.classList.remove('tab_active');
     }
-   let index = colors.findIndex((color) =>
-   color.classList.contains("color_active"));
-   let target = event.target;
-   event.preventDefault();
+  });
 
-colors[index].classList.remove("color_active");
-target.classList.toggle("color_active")
-bookControl.classList.remove("book_color-black", "book_color-gray", "book_color-whitesmoke")
-if (target.classList.contains("text_color_black")){
-    bookControl.classList.add("book_color-black")  
-} else if (target.classList.contains("text_color_gray")){
-    bookControl.classList.add("book_color-gray")
-} else if (target.classList.contains("text_color_whitesmoke")){
-    bookControl.classList.add("book_color-whitesmoke")
-}
-}
-
-
-const bgColorChange = function(event){
-    if (event.target.tagName == 'SPAN') {
-        return
+  tabContents.forEach((content, index) => {
+    if (index === tabIndex) {
+      content.classList.add('tab__content_active');
+    } else {
+      content.classList.remove('tab__content_active');
     }
-   let index = bgColors.findIndex((color) =>
-   color.classList.contains("color_active"));
-   let target = event.target;
-   event.preventDefault();
-
-bgColors[index].classList.remove("color_active");
-target.classList.toggle("color_active")
-bookControl.classList.remove("book_bg-gray", "book_bg-black", "book_bg-white")
-if (target.classList.contains("bg_color_gray")){
-    bookControl.classList.add("book_bg-gray")  
-} else if (target.classList.contains("bg_color_black")){
-    bookControl.classList.add("book_bg-black")
-} else if (target.classList.contains("bg_color_white")){
-    bookControl.classList.add("book_bg-white")
-}
+  });
 }
 
-
-
-fontControl.addEventListener("click", fontsizeChange)
-colorControl.addEventListener("click", colorChange)
-bgControl.addEventListener("click", bgColorChange)
+tabs.forEach((tab, index) => {
+  tab.addEventListener('click', () => {
+    activateTab(index);
+  });
+});
